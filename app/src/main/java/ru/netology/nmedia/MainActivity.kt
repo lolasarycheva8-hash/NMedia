54ee0099f2a604c21be0da2a2f69934235c82be4
Sarycheva Lola <lolasarycheva8@gmail.com>
2026-07-22 13:39:50 +0300

    Add likes, shares, views with number formatting

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,20 +1,47 @@
 package ru.netology.nmedia
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
+    private var likes = 10
+    private var isLiked = false
+    private var shares = 5
+    private val views = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnLike = findViewById<ImageButton>(R.id.btnLike)
+        val tvLikes = findViewById<TextView>(R.id.tvLikes)
+        val btnShare = findViewById<ImageButton>(R.id.btnShare)
+        val tvShares = findViewById<TextView>(R.id.tvShares)
+        val tvViews = findViewById<TextView>(R.id.tvViews)
+        tvLikes.text = formatCount(likes)
+        tvShares.text = formatCount(shares)
+        tvViews.text = formatCount(views)
+        btnLike.setOnClickListener {
+            if (isLiked) {
+                likes--
+                isLiked = false
+                btnLike.setImageResource(R.drawable.ic_like)
+            } else {
+                likes++
+                isLiked = true
+                btnLike.setImageResource(R.drawable.ic_like_filled)
+            }
+            tvLikes.text = formatCount(likes)
+        }
+        btnShare.setOnClickListener {
+            shares++
+            tvShares.text = formatCount(shares)
+        }
+    }
+    fun formatCount(count: Int): String {
+        return when {
+            count < 1_000 -> count.toString()
+            count < 10_000 -> "${count / 1_000}.${(count % 1_000) / 100}K"
+            count < 1_000_000 -> "${count / 1_000}K"
+            else -> "${count / 1_000_000}.${(count % 1_000_000) / 100_000}M"
         }
     }
 }
